7b45947d700ea7909c45bc7dbdb09c3a7386994c
BrenoIO <115326553+BrenoIO@users.noreply.github.com>
2023-06-22 16:27:01 -0300

    Vereador

---
+++ Vereador/Votacao/Alderman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votacao
+{
+    internal class Alderman
+    {
+        public string Name { get; private set; }
+        public int Age { get; set; }
+        public string PoliticalParty { get; set; }
+        
+
+
+        public Alderman(string name, int age, string politicalParty)
+        {
+            Name = name;
+            Age = age;
+            PoliticalParty = politicalParty;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Name}\nIdade: {Age}\nPartido: {PoliticalParty}\n";
+        }
+    }
+
+
+}
---
+++ Vereador/Votacao/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Globalization;
+using Votacao;
+
+class program
+{
+    static List<Alderman> aldermens = new List<Alderman>();
+
+    static void Main(string[] args)
+    {
+        bool getout = false;
+
+        while (!getout)
+        {
+            Console.WriteLine("Olá seja bem vindo ao sistema de cadastro de vereadores da cidade de Martelandia");
+            Console.WriteLine("1 - Cadastrar vereador");
+            Console.WriteLine("2 - Listar vereadores");
+            Console.WriteLine("3 - Sair");
+            Console.WriteLine("Escolha uma opção");
+            string option = Console.ReadLine();
+
+            Console.Clear();
+
+            switch (option)
+            {
+                case "1":
+                    RegisterAlderman();
+                    break;
+                case "2":
+                    ListAlderman();
+                    break;
+                case "3":
+                    getout = true;
+                    Console.WriteLine("Obrigado por usar o programa");
+                    Console.WriteLine("Saindo do programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opção invalida!");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+    static void RegisterAlderman ()
+    {
+        Console.WriteLine("===   Cadastro de Vereador   ===");
+        Console.Write("Nome: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Partido: ");
+        string politicalparty = Console.ReadLine();
+
+        Console.Write("Idade: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Alderman alderman = new Alderman(name, age, politicalparty);
+        aldermens.Add(alderman);
+
+        Console.WriteLine("Processo Concluido!!");
+        Console.WriteLine();
+        Console.WriteLine("Vereador cadastrado com sucesso!");
+
+    }
+
+    static void ListAlderman()
+    {
+        Console.WriteLine("===   Vereadores Cadastrados   ===");
+
+        if (aldermens.Count == 0)
+        {
+            Console.WriteLine("Nenhum vereador cadastrado");
+        }
+        else
+        {
+            foreach (Alderman alderman in aldermens)
+            {
+                Console.WriteLine(alderman);
+            }
+        }
+    }
+
+}
